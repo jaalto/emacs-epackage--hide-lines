f616193ee61012a7d22a0f293c8effbdbef93aad
Jari Aalto <jari.aalto@cante.net>
2011-12-07 11:29:50 +0200

    epackage/: Change provide to *-epkg-*
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- epackage/hide-lines-autoloads.el
+++ epackage/hide-lines-autoloads.el
@@ -2,4 +2,4 @@
 (autoload 'hide-non-matching-lines              "hide-lines" "" t)
 (autoload 'hide-matching-lines                  "hide-lines" "" t)
 (autoload 'show-all-invisible                   "hide-lines" "" t)
-(provide 'hide-lines-xinstall)
+(provide 'hide-lines-epkg-xinstall)
